e868658ca523d1792f3f6d24b4ac69c0f23c2259
shubhamPrakashJha <sjonline4u@gmail.com>
2017-09-11 19:10:12 +0530

    importing entire rest folder from twilio package

--- text-send.py
+++ text-send.py
@@ -1,11 +1,11 @@
-from twilio.rest import Client
+from twilio import rest
 
 # Your Account SID from twilio.com/console
 account_sid = "ACd96a890e0e24c2f333a00ece5a9c1849"
 # Your Auth Token from twilio.com/console
 auth_token  = "c62240dbf1d1536a987b1728d1f8fce5"
 
-client = Client(account_sid, auth_token)
+client = rest.Client(account_sid, auth_token)
 
 message = client.messages.create(
     to="+917992485554",
